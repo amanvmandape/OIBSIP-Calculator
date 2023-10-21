8ad3a6c8ed8b7967372b05aca2f16ed7a52d3efc
Aman Mandape <95904603+amanvmandape@users.noreply.github.com>
2023-10-21 15:15:40 +0530

    Initial Version

---
+++ script.js
@@ -0,0 +1,16 @@
+function addToScreen(event) {
+    document.getElementById('screen').value += event.target.value;
+  }
+
+  function clearScreen() {
+    document.getElementById('screen').value = '';
+  }
+
+  function calculateResult() {
+    try {
+      const result = eval(document.getElementById('screen').value);
+      document.getElementById('screen').value = result;
+    } catch (error) {
+      document.getElementById('screen').value = 'Incorrect Operation';
+    }
+  }
